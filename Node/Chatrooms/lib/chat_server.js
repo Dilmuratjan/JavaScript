7d616e025121f92ee5fd745688558ce7e1e06501
Dilmuratjohn <907568286@qq.com>
2018-05-27 15:35:47 +0800

    some changes

--- Node/Chatrooms/lib/chat_server.js
+++ Node/Chatrooms/lib/chat_server.js
@@ -1,140 +1,147 @@
 var socketio = require('socket.io');
 var io;
-var guestNumber = 1;
+var guestNumber = 0;
 var nickNames = {};
 var namesUsed = [];
 var currentRoom = {};
 //启动Socket.IO服务器
-exports.listen = function(server){
-	//启动Socket.IO服务器，允许它搭载在已有的HTTP服务器上
-	io = socketio.listen(server);
-	io.set = ('log level', 1);
-	//定义每个用户连接的处理逻辑
-	io.sockets.on('connection', function(socket){
-		//在用户连接上来时赋予其一个访客名
-		guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
-		//在用户连接上来时把他放入聊天室Lobby里面
-		joinRoom(socket, 'Lobby');
-		//处理用户的消息，更名，以及聊天室的创建和变更
-		handleMessageBroadcasting(siocket, nickNames);
-		handleNameChangeAttempts(socket, nickNames, namesUsed);
-		handleRoomJoining(socket);
-		//用户发出请求时，向其提供已经被占用的聊天室的列表
-		socket.on('rooms', function(){
-			socket.emit('rooms', io.sockets.manager.rooms);
-		});
-		//定义用户断开连接后的清除逻辑
-		handleClientDisconnection(socket, nickNames, namesUsed);
-	});
+exports.listen = function (server) {
+    //启动Socket.IO服务器，允许它搭载在已有的HTTP服务器上
+    io = socketio.listen(server);
+    io.set = ('log level', 1);
+    //定义每个用户连接的处理逻辑
+    io.sockets.on('connection', function (socket) {
+        //在用户连接上来时赋予其一个访客名
+        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+        //在用户连接上来时把他放入聊天室Lobby里面
+        joinRoom(socket, 'Lobby');
+        //处理用户的消息，更名，以及聊天室的创建和变更
+        handleMessageBroadcasting(socket, nickNames);
+        handleNameChangeAttempts(socket, nickNames, namesUsed);
+        handleRoomJoining(socket);
+        //用户发出请求时，向其提供已经被占用的聊天室的列表
+        socket.on('rooms', function () {
+            socket.emit('rooms', io.sockets.manager.rooms);
+        });
+        //定义用户断开连接后的清除逻辑
+        handleClientDisconnection(socket, nickNames, namesUsed);
+    });
 }
+
 //分配用户昵称
-function assignGuestName(socket, guestNumber, nickNames, namesUsed){
-	console.log('assignGuestName');
-	//生成新昵称
-	var name = 'Guest :' + guestNumber;
-	//将用户昵称与客户端连接ID关联
-	nickNames[socket.id] = name;
-	//让用户知道自己的昵称
-	socket.emit('nameResult', {
-		success:true,
-		name:name
-	});
-	//存放已被占用的昵称
-	namesUsed.push(name);
-	//增加用来生成昵称的计数器
-	return guestNumber + 1;
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+    console.log('assignGuestName');
+    //生成新昵称
+    var name = 'Guest :' + guestNumber;
+    console.log(name);
+    //将用户昵称与客户端连接ID关联
+    nickNames[socket.id] = name;
+    //让用户知道自己的昵称
+    socket.emit('nameResult', {
+        success: true,
+        name: name
+    });
+    //存放已被占用的昵称
+    namesUsed.push(name);
+    //增加用来生成昵称的计数器
+    return guestNumber + 1;
 }
+
 //与进入聊天室相关的逻辑
-function joinRoom(socket, room){
-	console.log('joinRoom');
-	//让用户进入房间
-	socket.join(room);
-	//记录用户的当前房间
-	currentRoom[socket.id] = room;
-	//让用户知道自己进入了新的房间
-	socket.emit('joinResult', {room: room});
-	//让房间里的其他用户得知有新用户进入了房间
-	socket.broadcast.to(room).emit('message', {
-		text:nickNames[socket.id] + ' has joined ' + room +'.'
-	});
-	//确定房间里有哪些用户
-	var userInRoom = io.socket.clients(room);
-	//如果有多个人，则汇总人数
-	if (userInRoom.length > 1) {
-		var userInRoomSummary = 'User currently in ' + room + ':';
-		for (var index in userInRoom) {
-			var userSocketId = userInRoom[index].id;
-			if(userSocketId != socket.id){
-				if (index > 0){
-				userInRoomSummary += ', ';
-				}
-			userInRoomSummary += nickNames [userSocketId];
-		}
-	}
-	userInRoomSummary += '.';
-	//将汇总信息发送给这个用户
-	socket.emit('message', {text: userInRoomSummary});
-	}
+function joinRoom(socket, room) {
+    console.log('joinRoom');
+    //让用户进入房间
+    socket.join(room);
+    //记录用户的当前房间
+    currentRoom[socket.id] = room;
+    //让用户知道自己进入了新的房间
+    socket.emit('joinResult', {room: room});
+    //让房间里的其他用户得知有新用户进入了房间
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + ' has joined ' + room + '.'
+    });
+    //确定房间里有哪些用户
+    var userInRoom = io.sockets.clients(room);
+    //如果有多个人，则汇总人数
+    if (userInRoom.length > 1) {
+        var userInRoomSummary = 'User currently in ' + room + ':';
+        for (var index in userInRoom) {
+            var userSocketId = userInRoom[index].id;
+            if (userSocketId != socket.id) {
+                if (index > 0) {
+                    userInRoomSummary += ', ';
+                }
+                userInRoomSummary += nickNames [userSocketId];
+            }
+        }
+        userInRoomSummary += '.';
+        //将汇总信息发送给这个用户
+        socket.emit('message', {text: userInRoomSummary});
+    }
 }
+
 //更名请求的处理逻辑
-function handleNameChangeAttempts(socket, nickNames, namesUsed){
-	console.log('handleNameChangeAttempts');
-	//添加nameAttempt事件的监听器
-	socket.on('nameAttempt', function(name){
-		//昵称不能以Guest开头
-		if(name.indexOf('Guest') == 0) {
-			socket.emit('nameResult', {
-				success: false,
-				message: 'Name cannot begin with "Guest".'
-			});
-		} else {
-			//如果昵称未被占用则注册新昵称
-			if(namesUsed.indexOf(name) == -1){
-				var previousName = nickNames[socket.id];
-				var previousNameIndex = namesUsed.indexOf(previousName);
-				namesUsed.push(name);
-				nickNames[socket.id] = name;
-				//删除旧的昵称
-				delete namesUsed[previousNameIndex];
-				socket.emit('nameResult', {
-					success: true,
-					name: name,
-				});
-				socket.broadcast.to(currentRoom[socket.id]).emit('message', {
-					text:previousName + ' is now known as ' + name + '.'
-				});
-			} else {
-				//如果昵称已被占用，则发送错误信息给客户端
-				socket.emit('nameResult', {
-					success: false,
-					message: 'That name is already in use.'
-				});
-			}
-		}
-	});
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+    console.log('handleNameChangeAttempts');
+    //添加nameAttempt事件的监听器
+    socket.on('nameAttempt', function (name) {
+        //昵称不能以Guest开头
+        if (name.indexOf('Guest') == 0) {
+            socket.emit('nameResult', {
+                success: false,
+                message: 'Name cannot begin with "Guest".'
+            });
+        } else {
+            //如果昵称未被占用则注册新昵称
+            if (namesUsed.indexOf(name) == -1) {
+                var previousName = nickNames[socket.id];
+                var previousNameIndex = namesUsed.indexOf(previousName);
+                namesUsed.push(name);
+                nickNames[socket.id] = name;
+                //删除旧的昵称
+                delete namesUsed[previousNameIndex];
+                socket.emit('nameResult', {
+                    success: true,
+                    name: name,
+                });
+                socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+                    text: previousName + ' is now known as ' + name + '.'
+                });
+            } else {
+                //如果昵称已被占用，则发送错误信息给客户端
+                socket.emit('nameResult', {
+                    success: false,
+                    message: 'That name is already in use.'
+                });
+            }
+        }
+    });
 }
+
 //发送聊天消息
-function handleMessageBroadcasting(socket){
-	socket.on('message', function(message){
-		socket.broadcast.to(message.room).emit('message', {
-			text: nickNames[socket.id] + ': ' + message.text
-		});
-	});
+function handleMessageBroadcasting(socket) {
+    socket.on('message', function (message) {
+        socket.broadcast.to(message.room).emit('message', {
+            text: nickNames[socket.id] + ': ' + message.text
+        });
+    });
 }
+
 //创建房间
-function handleRoomJoining(socket){
-	socket.on('join', function(room){
-		socket.leave(currentRoom[socket.id]);
-		joinRoom(socket, room.newRoom);
-	});
+function handleRoomJoining(socket) {
+    socket.on('join', function (room) {
+        socket.leave(currentRoom[socket.id]);
+        joinRoom(socket, room.newRoom);
+    });
 }
+
 //用户断开连接
-function handleClientDisconnection(socket){
-	socket.on('disconnection', function(){
-		var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
-		delete namesUsed[nameIndex];
-		delete nickNames[socket.id];
-	});
+function handleClientDisconnection(socket) {
+    socket.on('disconnection', function () {
+        var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+        delete namesUsed[nameIndex];
+        delete nickNames[socket.id];
+    });
 }
 
 
--- Node/Chatrooms/public/javascripts/chat_ui.js
+++ Node/Chatrooms/public/javascripts/chat_ui.js
@@ -1,82 +1,84 @@
 //显示可疑文本数据
 function divEscapedContentElement(message) {
-	return $('<div></div>').text(message);
+    return $('<div></div>').text(message);
 }
+
 //显示系统创建的受信内容
 function divSystemContentElement(message) {
-	return $('<div></div>').html('<i>' + message + '</i>');
+    return $('<div></div>').html('<i>' + message + '</i>');
 }
+
 //处理用户原始输入
 function processUserInput(chatApp, socket) {
-	var message = $('#send-message').val();
-	var systemMessage;
-	//如果用户输入的内容以斜杠开头，将其视为聊天命令
-	if (message.chatApp(0) == '/') {
-		systemMessage = chatApp.processCommand(message);
-		if(systemMessage) {
-			$('#message').append(divSystemContentElement(systemMessage));
-		}
-	} else{
-		//将非命令输入广播给其他用户 
-		chatApp.sendMessage($('#room').text(), message);
-		$('#message').append(divEscapedContentElement(message));
-		$('#message').scrollTop($('#message').prop('scrollHeight'));
-	}
+    var message = $('#send-message').val();
+    var systemMessage;
+    //如果用户输入的内容以斜杠开头，将其视为聊天命令
+    if (message.chatApp(0) == '/') {
+        systemMessage = chatApp.processCommand(message);
+        if (systemMessage) {
+            $('#message').append(divSystemContentElement(systemMessage));
+        }
+    } else {
+        //将非命令输入广播给其他用户
+        chatApp.sendMessage($('#room').text(), message);
+        $('#message').append(divEscapedContentElement(message));
+        $('#message').scrollTop($('#message').prop('scrollHeight'));
+    }
 
-	$('#send-message').val('');
+    $('#send-message').val('');
 }
 
 var socket = io.connect();
 
-$(document).read(function(){
-	var chatApp = new Chat(socket);
+$(document).ready(function () {
+    var chatApp = new Chat(socket);
 
-	socket.on('nameResult', function(result){
-		var message;
+    socket.on('nameResult', function (result) {
+        var message;
 
-		if (result.success) {
-			message = 'You are now known as ' + result.name + '.';
-		} else {
-			message = result.message;
-		}
-		$('#message').,append(divSystemContentElement(message));
-	});
+        if (result.success) {
+            message = 'You are now known as ' + result.name + '.';
+        } else {
+            message = result.message;
+        }
+        $('#message').append(divSystemContentElement(message));
+    });
 
-	socket.on('joinResult', function(result){
-		$('#room').text(result.room);
-		$('#message').append(divSystemContentElement('Room changed.'));
-	});
+    socket.on('joinResult', function (result) {
+        $('#room').text(result.room);
+        $('#message').append(divSystemContentElement('Room changed.'));
+    });
 
-	socket.on('message', function(message){
-		var newElement = $('<div></div>').text(message.text);
-		$('#message').append(newElement);
-	});
+    socket.on('message', function (message) {
+        var newElement = $('<div></div>').text(message.text);
+        $('#message').append(newElement);
+    });
 
-	socket.on('rooms', function(rooms){
-		$('#room-list').empty();
+    socket.on('rooms', function (rooms) {
+        $('#room-list').empty();
 
-		for (var room in rooms){
-			room = room.substring(1, room.length);
-			if (room != ''){
-				$('#room-list').append(divEscapedContentElement(room));
-			}
-		}
+        for (var room in rooms) {
+            room = room.substring(1, room.length);
+            if (room != '') {
+                $('#room-list').append(divEscapedContentElement(room));
+            }
+        }
 
-		$('#room-list div').click(function(){
-			chatApp.processCommand('/join ' + $(this).text());
-			$('#send-message').focus();
-		});
-	});
+        $('#room-list div').click(function () {
+            chatApp.processCommand('/join ' + $(this).text());
+            $('#send-message').focus();
+        });
+    });
 
-	setInterval(function(){
-		socket.emit('rooms');
-	}, 1000);
+    setInterval(function () {
+        socket.emit('rooms');
+    }, 1000);
 
-	$('#send-message').focus();
+    $('#send-message').focus();
 
-	$('#send-message').submit(function(){
-		processUserInput(chatApp, socket);
-		return false;
-	});
+    $('#send-message').submit(function () {
+        processUserInput(chatApp, socket);
+        return false;
+    });
 });
 
